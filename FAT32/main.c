13a39734fa0681d3f9a863c951091883b1904b0b
Hanul <lunahc92@Hanuls-MacBook-Pro.local>
2017-01-18 15:28:37 +0900

    implement media r/w function & make function to show file's status

--- FAT32/main.c
+++ FAT32/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include "fat_filelib.h"
 
+#define MEDIA_PATH "/dev/disk1"
+
+int in_file;
+int out_file;
+
 int media_init()
 {
-    // ...
+    
+    if((in_file = open(MEDIA_PATH, O_RDONLY)) < 0)
+    {
+        perror("open in_file");
+        exit(1);
+    }
+    
+    if((out_file = open(MEDIA_PATH, O_WRONLY)) < 0)
+    {
+        perror("open out_file");
+        exit(1);
+    }
+    
     return 1;
 }
 
@@ -13,9 +30,13 @@ int media_read(unsigned long sector, unsigned char *buffer, unsigned long sector
 
     for (i=0;i<sector_count;i++)
     {
-        // ...
-        // Add platform specific sector (512 bytes) read code here
-        //..
+        lseek(in_file, 512*sector, SEEK_SET);
+        
+        if(read(in_file, buffer, 512) < 0)
+        {
+            perror("read");
+            goto leave;
+        }
 
         sector ++;
         buffer += 512;
@@ -30,9 +51,13 @@ int media_write(unsigned long sector, unsigned char *buffer, unsigned long secto
 
     for (i=0;i<sector_count;i++)
     {
-        // ...
-        // Add platform specific sector (512 bytes) write code here
-        //..
+        lseek(out_file, 512*sector, SEEK_SET);
+        
+        if(write(out_file, buffer, 512) < 0)
+        {
+            perror("write");
+            goto leave;
+        }
 
         sector ++;
         buffer += 512;
@@ -41,7 +66,25 @@ int media_write(unsigned long sector, unsigned char *buffer, unsigned long secto
     return 1;
 }
 
-void main()
+void show_file_status(FL_FILE *file){
+    
+    printf("\nshortfile name = %s\n", file->shortfilename);
+    printf("file name = %s\n", file->filename);
+    printf("path = %s\n", file->path);
+    printf("parentcluster = %d\n", file->parentcluster);
+    printf("startcluster = %d\n", file->startcluster);
+    printf("bytenum = %d\n", file->bytenum);
+    printf("filelength = %d\n", file->filelength);
+    printf("filelength_changed = %d\n", file->filelength_changed);
+    
+    printf("file_data_address = %d\n", file->file_data_address);
+    printf("file_data_dirty = %d\n", file->file_data_dirty);
+    
+}
+
+
+
+int main(void)
 {
     FL_FILE *file;
 
@@ -55,7 +98,7 @@ void main()
     if (fl_attach_media(media_read, media_write) != FAT_INIT_OK)
     {
         printf("ERROR: Media attach failed\n");
-        return; 
+        return 1;
     }
 
     // List root directory
@@ -84,4 +127,8 @@ void main()
     fl_listdirectory("/");
 
     fl_shutdown();
+    
+leave:
+    close(out_file);
+    close(in_file);
 }
