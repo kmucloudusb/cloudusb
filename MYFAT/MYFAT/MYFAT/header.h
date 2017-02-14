855b2d71bcdc76d3f357650f6cdb5470566d0818
Hanul <lunahc92@Hanuls-MacBook-Pro.local>
2017-02-14 11:40:11 +0900

    Support FL_FILE

---
+++ MYFAT/MYFAT/MYFAT/header.h
@@ -0,0 +1,65 @@
+#ifndef header_h
+#define header_h
+
+#include "fat_defs.h"
+#include "fat_opts.h"
+#include "fat_types.h"
+
+//
+//
+// FL_FILE
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+struct fat_node
+{
+    struct fat_node    *previous;
+    struct fat_node    *next;
+};
+
+struct fat_list
+{
+    struct fat_node    *head;
+    struct fat_node    *tail;
+};
+
+struct cluster_lookup
+{
+    uint32 ClusterIdx;
+    uint32 CurrentCluster;
+};
+
+typedef struct sFL_FILE
+{
+    uint32                  parentcluster;
+    uint32                  startcluster;
+    uint32                  bytenum;
+    uint32                  filelength;
+    int                     filelength_changed;
+    char                    path[FATFS_MAX_LONG_FILENAME];
+    char                    filename[FATFS_MAX_LONG_FILENAME];
+    uint8                   shortfilename[11];
+    
+    // Cluster Lookup
+    struct cluster_lookup   last_fat_lookup;
+    
+    // Read/Write sector buffer
+    uint8                   file_data_sector[FAT_SECTOR_SIZE];
+    uint32                  file_data_address;
+    int                     file_data_dirty;
+    
+    // File fopen flags
+    uint8                   flags;
+#define FILE_READ           (1 << 0)
+#define FILE_WRITE          (1 << 1)
+#define FILE_APPEND         (1 << 2)
+#define FILE_BINARY         (1 << 3)
+#define FILE_ERASE          (1 << 4)
+#define FILE_CREATE         (1 << 5)
+    
+    struct fat_node         list_node;
+} FL_FILE;
+
+#endif /* header_h */
--- MYFAT/MYFAT/MYFAT/main.c
+++ MYFAT/MYFAT/MYFAT/main.c
@@ -1,15 +1,85 @@
-//
-//  main.c
-//  MYFAT
-//
-//  Created by Hanul on 14/02/2017.
-//  Copyright © 2017 Hanul. All rights reserved.
-//
-
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "header.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int in_file;
+int out_file;
+
+int media_init(char *media_path);
+int media_read(unsigned long sector, unsigned char *buffer, unsigned long sector_count);
+int media_write(unsigned long sector, unsigned char *buffer, unsigned long sector_count);
+void media_close();
+
+int main(int argc, char *argv[])
+{
+    FL_FILE *file;
+
+    media_init("/Users/lunahc92/Desktop/test.dmg");
+    
+    media_close();
+}
+
+int media_init(char *media_path)
+{
+    if((in_file = open(media_path, O_RDONLY)) < 0)
+    {
+        perror(media_path);
+        exit(1);
+    }
+    
+    if((out_file = open(media_path, O_WRONLY)) < 0)
+    {
+        perror(media_path);
+        exit(1);
+    }
+    
+    return 1;
+}
+
+int media_read(unsigned long sector, unsigned char *buffer, unsigned long sector_count)
+{
+    unsigned long i;
+    
+    for (i=0;i<sector_count;i++)
+    {
+        lseek(in_file, 512*sector, SEEK_SET);
+        
+        if(read(in_file, buffer, 512) < 0)
+        {
+            perror("read");
+            return 0;
+        }
+        
+        sector ++;
+        buffer += 512;
+    }
+    
+    return 1;
+}
+
+int media_write(unsigned long sector, unsigned char *buffer, unsigned long sector_count)
+{
+    unsigned long i;
+    
+    for (i=0;i<sector_count;i++)
+    {
+        lseek(out_file, 512*sector, SEEK_SET);
+        
+        if(write(out_file, buffer, 512) < 0)
+        {
+            perror("write");
+            return 0;
+        }
+        
+        sector ++;
+        buffer += 512;
+    }
+    
+    return 1;
+}
+
+void media_close(){
+    close(out_file);
+    close(in_file);
 }
