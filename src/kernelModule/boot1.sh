c205120b9291cddf5cf9dbc1340335292289763e
lch01387 <lch01387@gmail.com>
2017-09-10 21:17:22 +0900

    file directory refectoring

--- src/kernelModule/boot1.sh
+++ src/kernelModule/boot1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sh /home/pi/for_pi/cloudusb/communicate_module/makenode.sh
-sudo insmod /home/pi/for_pi/cloudusb/communicate_module/connModule.ko
-/home/pi/MyCloudUSB/MYFAT/MYFAT/MYFAT/main.out &
+sh ./connect_module/makenode.sh
+sudo insmod ./connect_module/connModule.ko
+./../user_application/main.out &
