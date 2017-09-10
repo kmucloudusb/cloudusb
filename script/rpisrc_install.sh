3c550b3785a81e97d0353785ac6eeade7eb75ace
lch01387 <lch01387@gmail.com>
2017-09-10 14:32:25 +0900

    file directory refectoring

--- script/rpisrc_install.sh
+++
@@ -1,2 +0,0 @@
-#!cloudeusb/script
-sudo wget https://raw.githubusercontent.com/notro/rpi-source/master/rpi-source -O /usr/bin/rpi-source && sudo chmod +x /usr/bin/rpi-source && /usr/bin/rpi-source -q --tag-update
