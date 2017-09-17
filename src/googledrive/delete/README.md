214eb0125f8091b309de4790f076cc9e0fe1a278
HeesangJin <jinheesang@naver.com>
2017-09-17 14:53:54 +0900

    googleDrive: Add funtion 'delete'

---
+++ src/googledrive/delete/README.md
@@ -0,0 +1,4 @@
+Google Drive API를 이용한 파일 삭제(Python)
+---------
+
+Need to write
---
+++ src/googledrive/delete/delete.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import httplib2
+import os
+import sys
+import errno
+import io
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+file_id = ''
+
+try:
+    import argparse
+
+    tools.argparser.add_argument('--fid', default='-1', help='file id')
+
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+
+    if flags.fid:
+        file_id = flags.fid
+
+
+except ImportError:
+    flags = None
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+# 접근 권한: https://developers.google.com/drive/v2/web/about-auth
+SCOPES = 'https://www.googleapis.com/auth/drive' # all permision
+
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+FOLDER = "application/vnd.google-apps.folder"  # 구글 드라이브 API에선 타입이 이 스트링인 파일을 폴더로 인식함
+ROOT_FOLDER = "cloud_usb_test"  # 테스트를 위한 최상위 폴더
+
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def main():
+    # 구글 계정 권한 얻기
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    file_delete(service, file_id)
+
+def file_delete(service, file_id):
+    file = service.files().delete(fileId=file_id).execute()
+    print('File Delete: %s' % file_id)
+
+if __name__ == '__main__':
+    main()
