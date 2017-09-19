5707836abf18b0afd58bdf3ad01527fd0cc6d7bd
lch01387 <lch01387@gmail.com>
2017-09-19 16:56:15 +0900

    add siginfo

--- src/kernelModule/kernel_module/f_mass_storage.c
+++ src/kernelModule/kernel_module/f_mass_storage.c
@@ -731,6 +731,8 @@ static int do_read(struct fsg_common *common)
         /* send block request and wait until return file content. no race condition */
         while(cloud_flag==EXECUTE_READ || cloud_flag ==EXECUTE_WRITE ){schedule_timeout_uninterruptible(0.001*HZ);}
         printk(KERN_ALERT "CloudUSB_fmass after loop\n");
+        printk(KERN_ALERT "CloudUSB_fmass read_file_offset:%lld\n", read_file_offset);
+        printk(KERN_ALERT "CloudUSB_fmass read_amount:%u\n", read_amount);
         /* perform the read */
         /* remove this function and send block request */
          //nread = vfs_read(curlun->filp,(char __user *)bh->buf, amount, &file_offset_tmp);
